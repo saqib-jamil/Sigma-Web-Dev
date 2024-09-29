64b3f3bf891d3d4f6725043b2badfc5bb1d7f602
Saqib <tanolisaqib276@gmail.com>
2024-09-30 02:24:56 +0500

    Business Name Gen

---
+++ JS/BusNameGen.js
@@ -0,0 +1,32 @@
+
+let adj = {
+    1: "Crazy",
+    2: "Amazing",
+    3: "Fire"
+}
+
+let shopeName = {
+    1: "Engine",
+    2: "Food",
+    3: "Garment"
+}
+
+let word = {
+    1: "Bros",
+    2: "Limited",
+    3: "Hub"
+}
+
+let randNumb = Math.ceil(Math.random() * 3)
+let randNumb2 = Math.ceil(Math.random() * 3)
+let randNumb3 = Math.ceil(Math.random() * 3)
+
+
+
+let myFunc = () => {
+
+    let newName = `${adj[randNumb]}  ${shopeName[randNumb2]}  ${word[randNumb3]}`
+    console.log(newName)
+
+}
+myFunc()
---
+++ JS/strings.js
@@ -0,0 +1,9 @@
+
+let name1 = "SAQIB"
+
+console.log(name1.charAt(1))
+console.log(name1.toLocaleLowerCase())
+console.log(name1.length)
+console.log(name1.trim())
+console.log(name1.includes("S"))
+console.log(name1.endsWith("S"))
