a65911c1e97202c1fa797d9dfac803949a68ae07
Saqib <tanolisaqib276@gmail.com>
2024-10-01 22:20:21 +0500

    Factorial Done

---
+++ JS/Fact.js
@@ -0,0 +1,26 @@
+
+//  FACT USING REDUCE
+
+let fact1 = []
+
+for (let index = 1; index <= 6; index++) {
+    fact1.push(index)
+}
+let factLeng = fact1.length
+
+let result = fact1.reduce((one,two)=>{
+    return one * two
+})
+
+console.log(`The Factorial of ${factLeng} is ${result}`)
+
+//  FACT USING FOR LOOP
+let result1 = 1
+let j=5
+for (let i=j; i>1; i--) {
+    result1 *= i
+}
+
+console.log(`The Factorial of ${j} is ${result1}`)
+
+
